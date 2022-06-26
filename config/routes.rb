0d8a474cf79d52f30967bfe5ecc0090d5166128b
Jacob Yaw <yawjacob@yahoo.com>
2022-06-26 16:34:59 -0500

    commit tree 9c035a83cbd8ac7b898ea3c5a0e3651707b57511

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
-  get '/plants', to: 'plants#index'
-  get '/plants/:id', to: 'plants#show'
-  post '/plants', to: 'plants#create'
+  # get '/plants', to: 'plants#index'
+  # get '/plants/:id', to: 'plants#show'
+  # post '/plants', to: 'plants#create'
+
+  resources :plants, only: [:index, :show, :create]
 
 end
